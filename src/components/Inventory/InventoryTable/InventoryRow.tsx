1a307c48314ac2a040399ce3faedccb80d19694a
Jessica Willard <jessica.bougie@lantidote.com>
2025-09-06 21:48:47 -0400

    refactor: improve inventory table styles and optimize InventoryRow component
    - Adjusted padding and text sizes in inventory table header and cell styles for better responsiveness.
    - Introduced width variants for inventory table cells to enhance layout flexibility.
    - Simplified state management in InventoryRow by initializing low stock state directly from props.
    - Updated InventoryRow to utilize new width variants for improved cell alignment and presentation.

--- src/components/Inventory/InventoryTable/InventoryRow.tsx
+++ src/components/Inventory/InventoryTable/InventoryRow.tsx
@@ -6,7 +6,7 @@ import {
   inventoryTableQuantityStyles,
   inventoryTableQuantityLowStyles,
 } from "../theme";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { formatDateFromString } from "@/utils/date";
 
 interface InventoryRowProps extends InventoryProps {
@@ -27,12 +27,8 @@ export const InventoryRow = (props: InventoryRowProps) => {
     onRowClick,
   } = props;
 
-  const [isLow, setIsLow] = useState(false);
   const lowStock = parseFloat(quantity) <= parseFloat(minCount || "0");
-
-  useEffect(() => {
-    setIsLow(lowStock);
-  }, [lowStock]);
+  const [isLow, setIsLow] = useState(lowStock);
 
   const totalPrice = pricePerUnit
     ? parseFloat(pricePerUnit) * parseFloat(quantity)
@@ -52,7 +48,9 @@ export const InventoryRow = (props: InventoryRowProps) => {
       })}
       onClick={handleRowClick}
     >
-      <td className={inventoryTableCellStyles({ variant: type })}>
+      <td
+        className={inventoryTableCellStyles({ variant: type, width: "name" })}
+      >
         <div>{name && <div className="font-medium">{name}</div>}</div>
       </td>
       <td className={inventoryTableCellStyles({ variant: type })}>
@@ -61,24 +59,37 @@ export const InventoryRow = (props: InventoryRowProps) => {
       <td
         className={inventoryTableCellStyles({
           variant: type,
+          width: "quantity",
+          // align: "center",
         })}
       >
         <span className={inventoryTableQuantityStyles({ variant: type })}>
           {parseFloat(quantity)}
         </span>
       </td>
-      <td className={inventoryTableCellStyles({ variant: type })}>
+      <td
+        className={inventoryTableCellStyles({ variant: type, width: "date" })}
+      >
         {formatDateFromString(countDate)}
       </td>
       <td className={inventoryTableCellStyles({ variant: type })}>
         {pricePerUnit ? `$${parseFloat(pricePerUnit).toFixed(2)}` : "-"}
       </td>
-      <td className={inventoryTableCellStyles({ variant: type })}>
-        {totalPrice > 0 ? `$${totalPrice.toFixed(2)}` : "-"}
+      <td
+        className={`${inventoryTableCellStyles({
+          variant: type,
+        })} pr-4`}
+      >
+        <div className="relative">
+          {totalPrice > 0 ? `$${totalPrice.toFixed(2)}` : "-"}
+          {isLow && (
+            <Icon
+              name="LowStock"
+              className={inventoryTableQuantityLowStyles()}
+            />
+          )}
+        </div>
       </td>
-      {isLow && (
-        <Icon name="LowStock" className={inventoryTableQuantityLowStyles()} />
-      )}
     </tr>
   );
 };
